3a3884e44581a677a11a489364f17ea3b5c1d61a
4n6Track <basedexpert00@gmail.com>
2026-08-18 12:13:46 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 // firebaseConfig 가 아직 채워지지 않았으면 온라인 기능(계정/리더보드)을 비활성화하고
 // 퍼즐 플레이 자체는 오프라인으로 계속 동작하도록 한다.
 export const isFirebaseConfigured =
-  !firebaseConfig.apiKey.startsWith('YOUR_');
+  !firebaseConfig.apiKey.startsWith('AIzaSyBdCDFljBDwO0O27LfAu2VTRbl6r7tdvtk');
 
 export let app = null;
 export let auth = null;
